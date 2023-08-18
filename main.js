ca13e709f36e1cbbd933f1ba3414b1284599f55a
luoar rian <rian.luoar@gmail.com>
2023-08-18 15:44:09 +0530

    for loop

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            varients: [
+                { id: 223, color: 'green' },
+                { id: 224, color: 'blue' }
+            ]
         }
     }
 })
